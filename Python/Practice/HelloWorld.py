2c8c0f857b339d924399ae33df71d3a4e13f4a2c
Aaron Robey <robey.aaron98@gmail.com>
2018-10-17 19:10:27 +0100

    Practiced some python

---
+++ Python/Practice/HelloWorld.py
@@ -0,0 +1,15 @@
+import math
+
+def square(number):
+    return number * number
+
+x = 0
+y = "Sally"
+print("Hello World")
+for x in y: #Loops x through every value in y
+    print(x)
+for x in range(10): #Loops x from 0 to 9
+    print(x)
+print(y)
+print(square(5))
+print(math.sqrt(square(5)))
